0a60756d2947a37b5ff1ee6f1950346814073a5b
jsj1027 <jsj1027@g.rit.edu>
2019-07-18 22:19:14 -0400

    read in from std in, not that bad just forgot the expect.

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,25 @@ fn main() {
         example: String,
     }
 
-    let story = Workproduct{
+    let story = Workproduct {
         name: String::from("Story"),
         example: String::from("S12345"),
     };
-    
-    let defect = Workproduct{
+
+    let defect = Workproduct {
         name: String::from("Defect"),
         example: String::from("DE12345"),
     };
-    
-    println!("What is the work product? ({},{}) Examples:({},{})", story.name, defect.name, story.example, defect.example);
+
+    let mut work_product = String::new();
+    println!(
+        "What is the work product? ({},{}) Examples:({},{})",
+        story.name, defect.name, story.example, defect.example
+    );
+
+    io::stdin()
+        .read_line(&mut work_product)
+        .expect("Failed to read work product");
+
+    println!("Work product is {}", &work_product);
 }
