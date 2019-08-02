4732fc1816d036a1e40386d859847af27af02577
Javis Johnson <jsj11027@g.rit.edu>
2019-08-02 15:13:32 -0400

    working on rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,31 @@
-use std::io::stdin;
-
-fn check_in_string(array_example: &[String], string: &String) -> bool {
-    let mut check = false;
-    for item in array_example.iter() {
-        if item == string {
-            check = true;
-        }
-    }
-    check
-}
+use std::io;
 
 fn main() {
-    let mut last: [&str] = [];
+    let mut work_product = String::new();
+    
+    println!("Enter the work product you ware working on. Example: S1234, DE1234");
 
-    let wp_array: [&'static str; 2] = ["Story", "Defect"];
-    let wp_array_examples: [&'static str; 2] = ["S", "D"];
-
-    let type_array: [&'static str; 5] = [
-        "Acceptance",
-        "Regression",
-        "Manual",
-        "User Interface",
-        "Usability",
-    ];
-    let type_array_examples: [&'static str; 5] = ["A", "R", "M", "UI", "U"];
-
-    let mut work_product = &'static str;
-
-    println!(
-        "What is the work product? {} Examples:{}",
-        &wp_array, wp_array_examples
-    );
-
-    stdin()
+    io::stdin()
         .read_line(&mut work_product)
         .expect("Failed to read work product");
 
-    let mut work_product = work_product.trim().to_uppercase();
+    let mut number_of_test_cases = String::new();
 
-    let check = check_in_string(&wp_array_examples[0..1], &work_product);
+    loop {
+        println!("How many test cases do you need?");
 
-    if !check {
-        println!("Wrong work product");
+        io::stdin()
+            .read_line(&mut number_of_test_cases)
+            .expect("Failed to read number of test cases");
+
+        let number_of_test_cases: u32 = number_of_test_cases.trim().parse();
+
+        match number_of_test_cases {
+            Ok(num) => {break},
+            Err(_) => {
+                println!("Not a number");
+                continue
+            },
+        };
     }
-    else {
-        last.push(&work_product);
-    }
-
-    println!(
-        "What is the type of test? {:?} Examples:{:?}",
-        type_array, type_array_examples
-    );
-
-    stdin()
-        .read_line(&mut work_product)
-        .expect("Failed to read test type");
-
-    // if wp_array_examples.contains(& work_product) {}
-
-    // println!("What is the type of test that you are writing? ({},{}) Examples:({},{})",);
 }
-//testfolder
-//Type Acceptance Regression Manual UserInterface Usability
-//Method Manual Automated
-//Project Elements
